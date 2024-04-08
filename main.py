bf53ec75299378776f4ca8689febac109d7f7eb6
Sriraj Behera <behera.sriraj2911@gmail.com>
2024-04-09 02:32:18 +0530

    added cors

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.student import student_router
 
 app = FastAPI()
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PATCH", "DELETE"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
