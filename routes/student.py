7dda54fd35f2ac50d4fb12eb2880ad607a72ce9a
Sriraj Behera <behera.sriraj2911@gmail.com>
2024-04-08 14:37:23 +0530

    added requirements.txt + student api desc

--- routes/student.py
+++ routes/student.py
@@ -8,16 +8,22 @@ from schemas.student import student_entity, students_entity
 student_router = APIRouter()
 
 
-@student_router.post("/students", status_code=status.HTTP_201_CREATED)
+@student_router.post("/students",
+                     description="API to create a student in the system. All fields are mandatory and required while creating the student in the system.",
+                     status_code=status.HTTP_201_CREATED,
+                     response_description="A JSON response sending back the ID of the newly created student record.",
+                     )
 async def create_student(student: Student):
     student_id = await student_collection.insert_one(student.dict())
     return {"id": str(student_id.inserted_id)}
 
 
-@student_router.get("/students", status_code=status.HTTP_200_OK)
+@student_router.get("/students", status_code=status.HTTP_200_OK, description="An API to find a list of students. You can apply filters on this API by passing the query parameters as listed below.")
 async def list_students(
-        country: Optional[str] = Query(None, description="To apply filter of country. If not given or empty, this filter should be applied."),
-        age: Optional[int] = Query(None, description="Only records which have age greater than equal to the provided age should be present in the result. If not given or empty, this filter should be applied.")
+        country: Optional[str] = Query(None,
+                                       description="To apply filter of country. If not given or empty, this filter should be applied."),
+        age: Optional[int] = Query(None,
+                                   description="Only records which have age greater than equal to the provided age should be present in the result. If not given or empty, this filter should be applied.")
 ):
     query = {}
     if country:
@@ -37,7 +43,7 @@ async def get_student(id: str = Path(..., description="The ID of the student pre
     return student_entity(student)
 
 
-@student_router.patch("/students/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@student_router.patch("/students/{id}", status_code=status.HTTP_204_NO_CONTENT, description="API to update the student's properties based on information provided. Not mandatory that all information would be sent in PATCH, only what fields are sent should be updated in the Database.")
 async def update_student(
         id: str = Path(..., description="The ID of the student previously created."),
         student_update: Student = {}
